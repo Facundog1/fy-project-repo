30da6281148b464ee5bbc004a0e34c585db23b9d
Facundo Dawson <f.dawson66@gmail.com>
2023-05-30 20:41:44 -0500

    changed rating and added gifs

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,18 +16,24 @@ function getWeather(cityname) {
         .then(function(data) {
             console.log(data)
             temperatureSpan.innerText = data.main.temp;
+            getGIF(data.weather[0].main);
         })
 }
 
 
 function getGIF(weather) {
-    var giphyURL = `https://api.giphy.com/v1/gifs/search?q=${weather}&api_key=${giphyAPIKey}`
+    console.log(weather);
+    var giphyURL = `https://api.giphy.com/v1/gifs/search?q=${weather}+weather&api_key=${giphyAPIKey}&rating=pg-13`
     fetch(giphyURL)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
             console.log(data)
+            var imgEl = document.createElement('img');
+            imgEl.src = data.data[0].images.fixed_height.url;
+            imgEl.alt = data.data[0].title;
+            document.body.appendChild(imgEl);
         })
 }
 

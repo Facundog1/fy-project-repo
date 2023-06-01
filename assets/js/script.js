bbaaef96712bfe78d52197fcf5f2e1b607edbdbb
yuski <yuskijoness@gmail.com>
2023-05-31 21:01:10 -0400

    added bulma,fixed the way weather is fetched, added high and low temps for the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
  var giphyAPIKey = "sDv42NVlG2ZVPXsd3YNYJKouegkM1X18";
  var weatherAPIkey = "fed330d490f63f8a5e01b53eaa09b02c"
 
-var searchForm = document.getElementById("search-form")
+var searchForm = document.getElementById("search-form");
 var gifForm = document.getElementById("gif-form");
 var temperatureSpan = document.getElementById("temperature");
+var highTempSpan = document.getElementById("High-Temp");
+var lowTempSpan = document.getElementById("Low-Temp");
+var weatherIcon = document.getElementById("weather-Icon");
+var conditons = document.getElementById("conditions");
+var weatherImage = document.getElementById("weather-Image");
 
 console.log(searchForm);
 
@@ -16,6 +21,10 @@ function getWeather(cityname) {
         .then(function(data) {
             console.log(data)
             temperatureSpan.innerText = data.main.temp;
+            highTempSpan.innerText = data.main.temp_max;
+            lowTempSpan.innerText = data.main.temp_min
+            weatherIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+            conditions.innerText = data.weather[0].description;
             getGIF(data.weather[0].main);
         })
 }
@@ -23,31 +32,41 @@ function getWeather(cityname) {
 
 function getGIF(weather) {
     console.log(weather);
-    var giphyURL = `https://api.giphy.com/v1/gifs/search?q=${weather}+weather&api_key=${giphyAPIKey}&rating=pg-13`
+    var giphyURL = `https://api.giphy.com/v1/gifs/search?q=${weather}+sky+weather&api_key=${giphyAPIKey}&rating=pg-13`
     fetch(giphyURL)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
             console.log(data)
-            var imgEl = document.createElement('img');
-            imgEl.src = data.data[0].images.fixed_height.url;
-            imgEl.alt = data.data[0].title;
-            document.body.appendChild(imgEl);
+            // var oldWeatherPic = document.getElementById("weather-picture");
+            // if (oldWeatherPic){
+            //     oldWeatherPic.remove();
+            // }
+            // var imgEl = document.createElement('img');
+            // imgEl.id = "weather-picture";
+            var imgUrl = data.data[0].images.fixed_height.url;
+            var alt = data.data[0].title;
+            weatherImage.src = imgUrl;
+            weatherImage.alt = alt;
+            // he wants us to do it in our own way and also figure out how to make images go away and maybe find more weather focused gifs.
+            // local storage 
+            // document.body.appendChild(imgEl);
+
         })
 }
 
 searchForm.addEventListener("submit",function(event){
     event.preventDefault() 
-    var searchInput = document.getElementById("search-input")
+    var searchInput = document.getElementById("city-input")
     console.log(searchInput)
     var cityName = searchInput.value.trim()
     getWeather(cityName)
 })
 
-gifForm.addEventListener("submit", function(event){
-    event.preventDefault();
-    var gifSearch = document.getElementById("gif-search");
-    var gifInput = gifSearch.value.trim();
-    getGIF(gifInput);
-})
+// gifForm.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     var gifSearch = document.getElementById("gif-search");
+//     var gifInput = gifSearch.value.trim();
+//     getGIF(gifInput);
+// })
